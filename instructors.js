2c063d9fa45c9b333d8a400da0c63a1d43fb700f
Nangiro <kira.fernando@gmail.com>
2021-01-25 17:05:16 -0300

    Manipulando dados (done)

--- instructors.js
+++ instructors.js
@@ -1,6 +1,47 @@
 const fs = require('fs')
 const data = require ('./data.json')
 
+//show
+exports.show = function(req,res) {
+    // req.params é usado paga pegar o id diretamete da url
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) {
+        return res.send("Instructor not found !!!")
+    }
+
+    function age (timestamp) {
+        const today = new Date() 
+        const birthday = new Date(timestamp)
+    
+        //ANO Exemplo: (2021 - 1993)
+        let age = today.getFullYear() - birthday.getFullYear()
+    
+        const month = today.getMonth() - birthday.getMonth()
+    
+    
+        //Verificando se ja passou o mes do aniversario
+        if (month < 0 || month == 0 && today.getDate() < birthday.getDate()) {
+            age = age-1
+        }
+        
+        return age
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","), //Trasnformar uma string em um array
+        created_at: "",
+    }
+
+    return res.render("instructors/show", {instructor})
+}
+
 //create
 exports.post = function(req,res) {
 
@@ -13,10 +54,21 @@ exports.post = function(req,res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, birth, name, services, gender} = req.body
 
-    data.instructors.push(req.body) //Adicione o req.body no meu array
+    birth = Date.parse(req.body.birth)
+    created_at = Date.now()
+    id = Number (data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        name,
+        avatar_url,
+        birth,
+        gender,
+        services,
+        created_at,
+    }) //Adicione o req.body no meu array
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Write file error")
